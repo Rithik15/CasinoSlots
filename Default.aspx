214aa678fc73391e502d0e6960e3abde4e34736c
Rithik Singla <97961911+Rithik15@users.noreply.github.com>
2023-09-15 09:41:22 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CasinoSlots.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Image ID="Image1" runat="server" Height="100px" Width="100px" />
+        <asp:Image ID="Image2" runat="server" Height="100px" Width="100px" />
+        <asp:Image ID="Image3" runat="server" Height="100px" Width="100px" />
+        <br />
+        <br />
+        Your Bet:
+        <asp:TextBox ID="TxtBet" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="BtnPull" runat="server" OnClick="BtnPull_Click" Text="Pull The Lever!" Width="105px" />
+        <br />
+        <br />
+        <asp:Label ID="LblResult" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="LblMoney" runat="server"></asp:Label>
+        <br />
+        <br />
+        1 Cherry -&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; X2 Your Bet<br />
+        2 Cherries -&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; X3 Your Bet<br />
+        3 Cherries -&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; X4 Your Bet<br />
+        3 HorseShoes - Jackpot - X100 Your Bet<br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CasinoSlots
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string[] images = new string[] { "cherry", "shamrock", "horseshoe" };
+        //this creates images[0]="cherry"
+        //             images[1]="shamrock"
+        //             images[2]="horseshoe"
+
+        Random r = new Random();  //creates an instance of the Random class   
+                                  //so we can generate random numbers below  
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnPull_Click(object sender, EventArgs e)
+        {
+            //when someone clicks the button, display a random image           
+
+            int bet = 0;
+
+            if (!int.TryParse(TxtBet.Text, out bet))
+            {
+                LblResult.Text = "Only integer bets please";
+                return;  //only accept integer bets
+            }
+
+            LblResult.Text = "";
+            LblMoney.Text = "";
+
+            PullLeverAndPlay(bet);            
+
+        }
+
+        private void PullLeverAndPlay(int b)
+        {
+            //pick numbers between 0 and 2
+            int slot1 = r.Next(0, 3);
+            int slot2 = r.Next(0, 3);
+            int slot3 = r.Next(0, 3);
+
+            //display the actual matching images on the page
+            Image1.ImageUrl = "/images/" + images[slot1] + ".jpg";
+            Image2.ImageUrl = "/images/" + images[slot2] + ".jpg";
+            Image3.ImageUrl = "/images/" + images[slot3] + ".jpg";
+
+            DetermineWinnings(slot1, slot2, slot3, b); //pass along the random numbers + AND the bet b to the method
+
+           
+        }
+
+        private void DetermineWinnings(int s1,int s2,int s3,int bt)
+        {
+            int w = 0;
+
+            if (s1 == 0 && s2 == 0 && s3 == 0) //three in a row cherries
+            {
+                w = bt * 4;
+                LblResult.Text = "Three Cherries";
+            }
+            else if ((s1 == 0 && s2 == 0) || (s1 == 0 && s3 == 0) //two cheeries ... three possible combinations
+                    || (s2 == 0 && s3 == 0))
+            {
+                w = bt * 3;
+                LblResult.Text = "Two Cherries";
+            }
+            else if ((s1 == 0 && (s2 != 0 && s3 != 0)) //one cherry ONLY .. so notice use of ! (NOT)
+                || (s2 == 0 && (s1 != 0 && s3 != 0))
+                || (s3 == 0 && (s1 != 0 && s2 != 0)))
+            {
+                w = bt * 2;
+                LblResult.Text = "One Cherry";
+            }
+            else if (s1 == 2 && s2 == 2 && s3 == 2)  //three in a row horshoes
+            {
+                w = bt * 100;
+                LblResult.Text = "Three Horshoes!!!";
+            }
+
+            LblMoney.Text = "You bet " + bt.ToString("c") + " and won " + w.ToString("c");
+        }
+        
+
+
+    }
+}
